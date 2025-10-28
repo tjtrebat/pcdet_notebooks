54eeea17f6bd15451ef161cfeab4b83ebc9cb349
Thomas J. Trebat <ttrebat3@gatech.edu>
2025-10-28 03:57:06 -0500

    added fov filtering

--- calibration_utils.py
+++ calibration_utils.py
@@ -32,3 +32,26 @@ def compute_box_3d(label, transform):
     ])
     corners_3d = R @ corners + np.array([[x], [y], [z]])
     return transform_points(corners_3d.T, transform)
+
+
+def convert_to_img_coords(points_velo, calib):
+    points_cam = transform_to_cam_coords(points_velo, calib)
+    points_cam_hom = np.vstack((points_cam, np.ones((1, points_cam.shape[1]))))
+    u, v, depth = project_to_image_plane(points_cam_hom, calib['P2'])
+    return np.stack((u, v, depth), axis=1)
+
+   
+def transform_to_cam_coords(points_velo, calib):
+    R0 = calib['R0']
+    Tr = calib['Tr_velo_to_cam']
+    points_hom = np.hstack((points_velo[:, :3], np.ones((points_velo.shape[0], 1))))
+    points_cam = (R0 @ (Tr @ points_hom.T)[:3, :])
+    return points_cam
+
+
+def project_to_image_plane(points_cam_hom, P2):
+    projections = P2 @ points_cam_hom
+    u = projections[0, :] / projections[2, :]
+    v = projections[1, :] / projections[2, :]
+    depth = projections[2, :] - P2[2, 3]
+    return u, v, depth
